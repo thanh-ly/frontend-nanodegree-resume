046507a84fdf85cc6f5b74680afdc93fc54a7d8a
thanh-ly <thanh.ly.1992@gmail.com>
2016-07-30 15:20:29 -0500

    incremental commit; cleaned up and organized resumeBuilder.js, encapsulated display functions for each object

--- js/resumeBuilder_legacy_0730.js
+++ js/resumeBuilder_legacy_0730.js
@@ -1,4 +1,6 @@
-//work object
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
 var work = {
     "jobs": [{
         "employer": "Gensler",
@@ -22,7 +24,6 @@ var work = {
     }]
 };
 
-//project object
 var projects = {
     "projects": [{
         "title": "Portfolio Website",
@@ -45,7 +46,6 @@ var projects = {
     },]
 };
 
-//bio object
 var bio = {
     "name": "Thanh Ly",
     "role": "front end developer",
@@ -60,7 +60,6 @@ var bio = {
     }
 };
 
-//education object
 var education = {
     "schools": [{
         "name": "UT Austin",
@@ -77,49 +76,77 @@ var education = {
     }]
 };
 
+var formattedName=HTMLheaderName.replace("%data%","Thanh Ly")
+var formattedRole=HTMLheaderRole.replace("%data%","Front End Developer")
+$("#header").prepend(formattedName, formattedRole);
 
-//bio.display encapsulation
-bio.display = function() {
-	var formattedPic=HTMLbioPic.replace("%data%", bio.biopic)
-	var formattedName=HTMLheaderName.replace("%data%", bio.name)
-	var formattedRole=HTMLheaderRole.replace("%data%", bio.role)
-	$("#header").prepend(formattedPic,formattedName,formattedRole);
+if(bio.skills.length > 0) {
 
-	if(bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
 
-			$("#header").append(HTMLskillsStart);
-
-			var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-			$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[1]);
-			$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[2]);
-			$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[3]);
-			$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[4]);
-			$("#skills").append(formattedSkill);
-	}
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+		$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[1]);
+		$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[2]);
+		$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[3]);
+		$("#skills").append(formattedSkill); formattedSkill = HTMLskills.replace("%data%",bio.skills[4]);
+		$("#skills").append(formattedSkill);
 };
 
-bio.display ();
-
-//work.display encapsulation
-work.display = function() {
+function displayWork() {
 	for (job in work.jobs) {
+		//creates a new div for work experience
 		$("#workExperience").append(HTMLworkStart);
 		//concatenates employer and title defined in work.jobs
 		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
 		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 		var formattedEmployerTitle = formattedEmployer + formattedTitle;
 		$(".work-entry:last").append(formattedEmployerTitle);
+		//add dates from work.jobs
 		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
 		$(".work-entry:last").append(formattedDates);
+		//add description from work.jobs
 		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
 		$(".work-entry:last").append(formattedDescription);
 	}
 };
 
-work.display();
+displayWork();
+$(document).click(function(loc) {
+  var x = loc.pageX;
+  var y = loc.pageY;
 
-//object.display encapsulation
+  logClicks(x,y);
+});
+
+// this function logs the location into an array and returns the location subobject in work object
+function locationizer(work_obj) {
+    var locationArray = []; 
+    
+    for (job in work_obj.jobs) {
+        var newLocation = work.jobs[job].location; 
+        locationArray.push(newLocation);
+    }
+    return locationArray;
+}
+
+// Did locationizer() work? This line will tell you!
+console.log(locationizer(work));
+
+function inName(name) {
+	name = bio.name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+	return name[0] + " " + name[1];
+}
+
+//html template string button, appending button to the DOM
+$("#main").append(internationalizeButton);
+
+
 projects.display = function() {
+
 	for (project in projects.projects) {
 		//console.log(project.length);
 		$("#projects").append(HTMLprojectStart);
@@ -141,15 +168,6 @@ projects.display = function() {
 };
 
 projects.display();
-
-
-//tracks and logs clicks on the DOM
-$(document).click(function(loc) {
-  var x = loc.pageX;
-  var y = loc.pageY;
-
-  logClicks(x,y);
-});
 
 //adds a google map with place markers for locations
 $("#mapDiv").append(googleMap);
